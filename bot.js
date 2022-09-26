8bc6218b1cd6e99b3948ac34cae0fdb73d57e805
Ivan <clicker.heroes.acg@gmail.com>
2022-09-26 17:48:22 +0300

    idk

---
+++ bot.js
@@ -0,0 +1,87 @@
+const {Telegraf} = require('telegraf')
+const express = require('express')
+const axios = require('axios')
+const path = require('path')
+const port = process.env.PORT || 3000
+const expressApp = express()
+const {logOnCommandError} = require("./src/log");
+const winston = require("winston");
+
+require('dotenv').config()
+
+winston.configure({
+  transports: [
+    new (winston.transports.File)({ filename: './logs/logging.log' })
+  ]
+})
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.command('start', async ctx => {
+  const greeting = 'Hello there! Welcome to RubyGems finder telegram bot!\nI respond to /latest /find + name of gem. Please try it';
+  bot.telegram.sendMessage(ctx.chat.id, greeting)
+  .then(() => console.log(ctx.from.id, 'greeting'))
+})
+
+const gemsList = (res, msg) => {
+  res.data
+  .slice(0, 10)
+  .forEach(gem => {
+    const info = gem.info.toString().replaceAll('\n', ' ')
+    msg += `\n*Gem:* ${gem.name}\n*Downloads:* ${gem.downloads}\n*Info:* ${info}\n*Link:* ${gem.project_uri}\n`
+  })
+  return msg;
+}
+
+bot.command('latest', async ctx => {
+  const id = ctx.chat.id;
+  let msg = 'There are 10 last gems:\n'
+  axios
+  .get('https://rubygems.org/api/v1/activity/just_updated.json')
+  .then(res => {
+    ctx.replyWithMarkdown(gemsList(res, msg))
+    .then(() => console.log(id, 'latest'))
+    .catch(err => logOnCommandError('latest', id, err, ctx))
+  })
+  .catch(err => logOnCommandError('latest', id, err, ctx))
+})
+
+bot.command('find', async ctx => {
+  const id = ctx.chat.id
+  const msgText = ctx.message.text
+  if (msgText === '/find') {
+    ctx
+    .replyWithMarkdown(
+        'You didn\'t specify a name of gem, try again please `/find gem`')
+    .then(() => console.log(id, 'empty name'))
+  } else {
+    const name = msgText.replaceAll('/find', '')
+    const uri = `https://rubygems.org/api/v1/search.json?query=${name}`
+    let msg = 'Search result:\n'
+    axios
+    .get(uri)
+    .then(res => {
+      msg = gemsList(res, msg)
+      ctx
+      .replyWithMarkdown(msg)
+      .then(() => console.log(id, 'success find'))
+      .catch(err => logOnCommandError('find', id, err, ctx))
+    })
+    .catch(err => logOnCommandError('find', id, err, ctx))
+  }
+})
+.catch(err => logOnCommandError('find', 'none', err))
+
+bot.command('quit', async ctx => {
+  const id = ctx.chat.id
+  ctx.telegram
+  .leaveChat(id).then(() => console.log(id, 'quit'))
+  .catch(err => {
+    logOnCommandError('quit', id, err)
+    bot.telegram.sendMessage(id, 'Unexpected error...')
+  })
+})
+
+bot.launch().then(() => console.log('bot started...'))
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
--- src/log.js
+++ src/log.js
@@ -1,12 +1,6 @@
 const winston = require("winston");
 
-winston.configure({
-  transports: [
-    new (winston.transports.File)({ filename: '../logs/logging.log' })
-  ]
-})
-
-function logOnCommandError(name, id, err) {
+function logOnCommandError(name, id, err, ctx) {
   Date.prototype.today = function () {
     return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
   }
@@ -14,7 +8,11 @@ function logOnCommandError(name, id, err) {
     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
   }
   const datetime = new Date().today() + " @ " + new Date().timeNow();
-  winston.error(`Chat_id: ${id} ${name}, date: ${datetime}, command `.concat(err.message))
+  console.error(err)
+  winston.error(`Chat_id: ${id}, Command: ${name}, Date: ${datetime}. `.concat(err.message))
+  if (ctx) {
+    ctx.reply("Unexpected error...")
+  }
 }
 
 module.exports = { logOnCommandError }
