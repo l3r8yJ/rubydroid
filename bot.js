db74bc61e55a66fc865dbb7610d36401caf4ae3c
Ivan <clicker.heroes.acg@gmail.com>
2022-09-26 23:48:48 +0300

    logging

--- bot.js
+++ bot.js
@@ -1,31 +1,32 @@
 const {Telegraf} = require('telegraf')
 const axios = require('axios')
-const {logOnCommandError} = require("./src/log");
+const {logOnCommandError, logInfo, now} = require("./src/log");
 const winston = require("winston");
-const {response} = require("express");
 
 require('dotenv').config()
 
 winston.configure({
-  transports: [new (winston.transports.File)({filename: './logs/logging.log'})]
+  transports: [new (winston.transports.File)({filename: `./logs/${now()}.log`})]
 })
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.command('start', async ctx => {
   const greeting = 'Hello there! Welcome to RubyGems finder telegram bot!\nI respond to /latest /find + name of gem. Please try it';
-  bot.telegram.sendMessage(ctx.chat.id, greeting)
-  .then(() => console.log(ctx.from.id, 'greeting'))
+  const id = ctx.chat.id;
+  bot.telegram.sendMessage(id, greeting)
+  .then(() => logInfo('start', id, `${id} registered`))
 })
 
 bot.command('latest', async ctx => {
   const id = ctx.chat.id;
   let msg = 'There are 10 last gems:\n'
+  logInfo('latest', id, 'Searching latest gems.', ctx)
   axios
   .get('https://rubygems.org/api/v1/activity/just_updated.json')
   .then(res => {
     ctx.replyWithHTML(gemsList(res, msg))
-    .then(() => console.log(id, 'latest'))
+    .then(() => logInfo('latest-success', id, 'latest found'))
     .catch(err => logOnCommandError('latest', id, err, ctx))
   })
   .catch(err => logOnCommandError('latest', id, err, ctx))
@@ -40,13 +41,14 @@ bot.command('find', async ctx => {
     const name = msgText.replaceAll('/find', '')
     const uri = `https://rubygems.org/api/v1/search.json?query=${name}`
     let msg = 'Search result:\n'
+    logInfo('find gems', id, `Searching gems with name "${name}"`, ctx)
     axios
     .get(uri)
     .then(res => {
       msg = gemsList(res, msg)
       ctx
       .replyWithHTML(msg)
-      .then(() => console.log(id, 'success find'))
+      .then(() => logInfo('find-success', id, `${name} found`))
       .catch(err => logOnCommandError('find', id, err, ctx))
     })
     .catch(err => logOnCommandError('find', id, err, ctx))
@@ -65,23 +67,40 @@ bot.command('deps', async ctx => {
     let msg = `Dependencies for \"${name}\":\n`
     let dev = `\n<b>Development:</b>\n`
     let run = `\n<b>Runtime:</b>\n`
+    logInfo('deps search', id, 'Searching dependencies.', ctx)
     axios
     .get(uri)
-    .then(async res => {
-      for (const dep of res.data.dependencies.development) {
-        dev += await depsAsHtml(dep);
-      }
-      for (const dep of res.data.dependencies.runtime) {
-        run += await depsAsHtml(dep)
-      }
-      ctx.replyWithHTML(msg.concat(dev).concat(run))
-      .catch(err => logOnCommandError('deps', id, err, ctx))
-    })
+    .then(
+        async res => {
+          dev = await accumDeps(
+              res.data.dependencies.development,
+              dev
+          )
+          run = await accumDeps(
+              res.data.dependencies.runtime,
+              run
+          )
+          ctx.replyWithHTML(msg.concat(dev).concat(run))
+          .catch(err => logOnCommandError('deps', id, err, ctx))
+          logInfo('deps-success', id, `${name} deps found`)
+        }
+    )
     .catch(err => logOnCommandError('deps', id, err, ctx))
   }
 })
 
-bot.launch().then(() => console.log('RubyRoid started!'))
+bot.launch().then(() => logInfo('Bot started', null, 'bot started'))
+
+const accumDeps = async (deps, accumulator) => {
+  if (deps.length !== 0) {
+    for (const dep of deps) {
+      accumulator += await depsAsHtml(dep);
+    }
+  } else {
+    accumulator += 'Dependencies not found...'
+  }
+  return accumulator;
+}
 
 const depsAsHtml = async (dep) => {
   const link = await axios
@@ -89,9 +108,8 @@ const depsAsHtml = async (dep) => {
   .then(response => {
     return response.data.project_uri;
   })
-  console.log(link)
   if (link === undefined) {
-    console.log('Data not presented!')
+    console.warn('Data not presented!')
   }
   return `<b>Name:</b> ${dep.name}, <b>link:</b> ${link}\n`;
 }
@@ -110,7 +128,7 @@ const sendNameWarningMessage = (ctx, id) => {
   ctx
   .replyWithMarkdown(
       'You didn\'t specify a name of gem, try again please `/command gemName`')
-  .then(() => console.log(id, 'empty name'))
+  .then(() => console.warn(id, 'empty name'))
 }
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
--- src/log.js
+++ src/log.js
@@ -1,18 +1,33 @@
 const winston = require("winston");
 
-function logOnCommandError(name, id, err, ctx) {
+const now = () => {
   Date.prototype.today = function () {
-    return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+    return ((this.getDate() < 10) ? "0" : "") + this.getDate() + "-"
+        + (((this.getMonth() + 1) < 10) ? "0" : "") + (this.getMonth() + 1)
+        + "-" + this.getFullYear();
   }
   Date.prototype.timeNow = function () {
-    return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+    return ((this.getHours() < 10) ? "0" : "") + this.getHours() + ":"
+        + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":"
+        + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds();
   }
-  const datetime = new Date().today() + " @ " + new Date().timeNow();
+  return new Date().today() + '-' + new Date().timeNow();
+}
+
+const logOnCommandError = (name, id, err, ctx = undefined) => {
   console.error(err)
-  winston.error(`Chat_id: ${id}, Command: ${name}, Date: ${datetime}. `.concat(err.message))
-  if (ctx) {
+  winston.error(`Chat_id: ${id}, Command: ${name}, Date: ${now()}. `.concat(err.message))
+  if (ctx !== undefined) {
     ctx.reply("Unexpected error...")
   }
 }
 
-module.exports = { logOnCommandError }
+const logInfo = (name, id, info, ctx = undefined) => {
+  console.info(name)
+  winston.info(`Chat_id: ${id}, Name: ${name}, Date: ${now()} `.concat(info))
+  if (ctx !== undefined) {
+    ctx.reply(info)
+  }
+}
+
+module.exports = { logOnCommandError, logInfo, now }
