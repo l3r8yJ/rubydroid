890c816b8e89c8ea19788e363ebe1b2259c4ce01
Ivan <clicker.heroes.acg@gmail.com>
2022-09-26 19:33:03 +0300

    deps

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const {Telegraf} = require('telegraf')
 const axios = require('axios')
 const {logOnCommandError} = require("./src/log");
 const winston = require("winston");
+const {response} = require("express");
 
 require('dotenv').config()
 
@@ -16,16 +17,6 @@ bot.command('start', async ctx => {
   bot.telegram.sendMessage(ctx.chat.id, greeting)
   .then(() => console.log(ctx.from.id, 'greeting'))
 })
-
-const gemsList = (res, msg) => {
-  res.data
-  .slice(0, 10)
-  .forEach(gem => {
-    const info = gem.info.toString().replaceAll('\n', ' ')
-    msg += `\n<b>Gem:</b> ${gem.name}\n<b>Downloads:</b> ${gem.downloads}\n<b>Info:</b> ${info}\n<b>Link:</b> ${gem.project_uri}\n`
-  })
-  return msg;
-}
 
 bot.command('latest', async ctx => {
   const id = ctx.chat.id;
@@ -44,10 +35,7 @@ bot.command('find', async ctx => {
   const id = ctx.chat.id
   const msgText = ctx.message.text
   if (msgText === '/find') {
-    ctx
-    .replyWithMarkdown(
-        'You didn\'t specify a name of gem, try again please `/find gem`')
-    .then(() => console.log(id, 'empty name'))
+    sendNameWarningMessage(ctx, id);
   } else {
     const name = msgText.replaceAll('/find', '')
     const uri = `https://rubygems.org/api/v1/search.json?query=${name}`
@@ -66,16 +54,64 @@ bot.command('find', async ctx => {
 })
 .catch(err => logOnCommandError('find', 'none', err))
 
-bot.command('quit', async ctx => {
+bot.command('deps', async ctx => {
   const id = ctx.chat.id
-  ctx.telegram
-  .leaveChat(id).then(() => console.log(id, 'quit'))
-  .catch(err => {
-    logOnCommandError('quit', id, err)
-    bot.telegram.sendMessage(id, 'Unexpected error...')
-  })
+  const msgText = ctx.message.text
+  if (msgText === '/deps') {
+    sendNameWarningMessage(ctx, id)
+  } else {
+    const name = msgText.replaceAll('/deps', '').replaceAll(' ', '')
+    const uri = `https://rubygems.org/api/v1/gems/${name}.json`
+    let msg = `Dependencies for \"${name}\":\n`
+    let dev = `\n<b>Development:</b>\n`
+    let run = `\n<b>Runtime:</b>\n`
+    axios
+    .get(uri)
+    .then(async res => {
+      for (const dep of res.data.dependencies.development) {
+        dev += await depsAsHtml(dep);
+      }
+      for (const dep of res.data.dependencies.runtime) {
+        run += await depsAsHtml(dep)
+      }
+      ctx.replyWithHTML(msg.concat(dev).concat(run))
+      .catch(err => logOnCommandError('deps', id, err, ctx))
+    })
+    .catch(err => logOnCommandError('deps', id, err, ctx))
+  }
 })
 
-bot.launch().then(() => console.log('bot started...'))
+bot.launch().then(() => console.log('RubyRoid started!'))
+
+const depsAsHtml = async (dep) => {
+  const link = await axios
+  .get(`https://rubygems.org/api/v1/gems/${dep.name}.json`)
+  .then(response => {
+    return response.data.project_uri;
+  })
+  console.log(link)
+  if (link === undefined) {
+    console.log('Data not presented!')
+  }
+  return `<b>Name:</b> ${dep.name}, <b>link:</b> ${link}\n`;
+}
+
+const gemsList = (res, msg) => {
+  res.data
+  .slice(0, 10)
+  .forEach(gem => {
+    const info = gem.info.toString().replaceAll('\n', ' ')
+    msg += `\n<b>Gem:</b> ${gem.name}\n<b>Downloads:</b> ${gem.downloads}\n<b>Info:</b> ${info}\n<b>Link:</b> ${gem.project_uri}\n`
+  })
+  return msg;
+}
+
+const sendNameWarningMessage = (ctx, id) => {
+  ctx
+  .replyWithMarkdown(
+      'You didn\'t specify a name of gem, try again please `/command gemName`')
+  .then(() => console.log(id, 'empty name'))
+}
+
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
